86dbcefb2ba69a9e9384302e09b4a4ae5a10322a
Abishek <abishek@Abisheks-MacBook-Air.local>
2025-08-05 03:28:15 -0500

    Volume Push

--- backend/main.py
+++ backend/main.py
@@ -10,13 +10,20 @@ image = (
     modal.Image.debian_slim()
     .apt_install("git")
     .pip_install_from_requirements("requirements.txt")
-    .run_commands(["git clone https://github.com/ace-step/ACE-Step.git /tmp/syntheziser-model", "cd /tmp/syntheziser-model && pip install ."])
+    .run_commands(["git clone https://github.com/ace-step/ACE-Step.git /tmp/synthesizer-model", "cd /tmp/synthesizer-model && pip install ."])
 
-    # Ensures when Modal app downloads a large model from Hugging Face for the first time, it saves it to a persistent cache. On all 
-    # subsequent runs, the app will find the model in that cache and load it directly, avoiding repeated downloads and significantly
-    # speeding up startup times.    
+    # Ensures when Modal app downloads a large model from Hugging Face for the first time, it saves it to a cache. 
+    # On all subsequent runs, the app will find the model in that cache and load it directly.    
                                                                  
-    .env({"HF_HOME" : "./cache/huggingface"}) 
+    .env({"HF_HOME" : "./cache/huggingface"})
+    .add_local_python_source("prompts") 
 )
+
+
+# Utilized modal.Volume to ensures qwen-2 and synthesizer model are downloaded & 
+# easy to share w/o re-downloading them again.
+
+model_volume = modal.Volume.from_name("synthesizer-model", create_if_missing=True)
+hf_volume = modal.Volume.from_name("qwen-2-hf-cache", create_if_missing=True)
 
 
